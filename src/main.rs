3cebe0e28b159b75006b11fa06a7e442ee0025e9
robesckey2k6 <robesckeyd@gmail.com>
2026-01-15 22:40:24 +0545

    fuckass rust just doesn't cooperate

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,36 @@ enum ClientState{
     OTHER
 }
 fn backward(server: Arc<Mutex<TcpStream>>, client: Arc<Mutex<TcpStream>>) {
+
+    let mut server_buffer = [0u8; 1024];
     loop {
-        let mut server_buffer = [0u8; 1024];
-        {
-            server.lock().unwrap().read(&mut server_buffer).unwrap();
+        {   
+            let data_read = server.lock().unwrap().read(&mut server_buffer).unwrap();
             println!("DATA RECV");
-            client.lock().unwrap().write_all(&server_buffer);
+            {
+            let mut tmp_client = client.lock().unwrap();
+            tmp_client.write_all(&server_buffer[..data_read]).unwrap(); // code stuck here? could client be
+                                                              // locked from somewhere else?
+            }
             println!("DATA SENT TO CLIENT");
         }
     }
 }
 fn handle_connection( mut stream: Arc<Mutex<TcpStream>>) {
 
-    let mut current_client_state: ClientState= ClientState::HANDSHAKE;
+    let mut current_client_state: ClientState = ClientState::HANDSHAKE;
 
     
+    // Server bound buffer
+    let mut buffer = [0u8; 1024];
+    let mut original_server_stream = Arc::new(Mutex::new(TcpStream::connect("127.0.0.1:25565").unwrap()));
     loop {
-        let mut buffer = [0u8; 1024];
-        stream.lock().unwrap().read(&mut buffer).unwrap(); // Getting data from stream
-        let mut server_stream = Arc::new(Mutex::new(TcpStream::connect("127.0.0.1:25565").unwrap()));
+        
+            // Getting data from stream
+           let data_read =  stream.lock().unwrap().read(&mut buffer).unwrap(); // Getting data from stream
+        
+
+        
 
         if current_client_state == ClientState::HANDSHAKE {
             let mut index = 0;
@@ -49,11 +60,13 @@ fn handle_connection( mut stream: Arc<Mutex<TcpStream>>) {
 
             println!("packet_length: {} packet_id: {} protocol_version: {}", packet_length, packet_id, protocol_version);
 
-            let mut server_stream_c = Arc::clone(&server_stream);
+            let mut server_stream_c = Arc::clone(&original_server_stream);
             let mut client_stream_c = Arc::clone(&stream);
             
-            
-            server_stream_c.lock().unwrap().write_all(&buffer).unwrap();
+            { 
+                let mut tmp_stream = server_stream_c.lock().unwrap();
+                tmp_stream.write_all(&buffer[..data_read]).unwrap();
+            }
 
             println!("BACK THREAD LOADing");
             let backward_thread_handle = thread::spawn(move || {
@@ -65,8 +78,13 @@ fn handle_connection( mut stream: Arc<Mutex<TcpStream>>) {
         }
         else {
             println!("SENDING DATA TO SERVER");
-            let mut server_stream_c = Arc::clone(&server_stream);
-            server_stream_c.lock().unwrap().write_all(&buffer).unwrap();
+            let mut server_stream_c = Arc::clone(&original_server_stream);
+
+            {
+                let mut tmp_ss = server_stream_c.lock().unwrap();
+                tmp_ss.write_all(&buffer[..data_read]).unwrap();
+            }
+            println!("DATA SENT TO SERVER");
         }
     }
 }
@@ -74,9 +92,13 @@ fn handle_connection( mut stream: Arc<Mutex<TcpStream>>) {
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("[INFO] Multiplexer started at 127.0.0.1:3000");
+
     for stream in listener.incoming() {
-        let client_stream = Arc::new(Mutex::new(stream.unwrap()));
-        let clone_stream = Arc::clone(&client_stream);
+        
+        // Cloning stream variable
+        let orginal_stream = Arc::new(Mutex::new(stream.unwrap()));
+        let clone_stream = Arc::clone(&orginal_stream);
+
         println!("[INFO] Connection established");
         handle_connection(clone_stream);
     }
