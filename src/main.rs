372efc5cc21419656ff9f940ef087203024b3ccd
robesckey2k6 <robesckeyd@gmail.com>
2026-01-17 21:21:47 +0545

    added redis db port lookup

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::thread;
 
+use redis::{Client, Commands};
+
+
+
 fn transfer(mut src: TcpStream,mut dest: TcpStream) {
     let mut src_buffer = [0u8; 1024];
     
@@ -23,7 +27,7 @@ fn transfer(mut src: TcpStream,mut dest: TcpStream) {
     }
 }
 
-fn handle_client(mut client: TcpStream) {
+fn handle_client(mut client: TcpStream, mut redis_instance: redis::Connection) {
 
     let mut client_buffer = [0u8; 1024];
     let mut rdat_len: usize;
@@ -34,9 +38,26 @@ fn handle_client(mut client: TcpStream) {
     let (_, _, _, svadr) = utils::packet::parse_handshake_data(&client_buffer);
     
     // TODO setup database configuration for address -> port determination
+    let parts: Vec<&str> = svadr.split('.').collect();
+
+    
+    let key_exist: bool = redis_instance.exists(parts[0]).unwrap();
+    
+    let port: String;
+
+    if key_exist {
+        port = redis_instance.get(parts[0]).unwrap();
+    }
+    else {
+        client.shutdown(std::net::Shutdown::Both).unwrap();
+        return;
+    }
 
     // Create server connection
-    let mut server = TcpStream::connect("127.0.0.1:25565").unwrap();
+    let mut server = TcpStream::connect(format!("127.0.0.1:{}", port)).unwrap();
+
+    println!("Connection established {} -> 127.0.0.1:{}", svadr, port);
+    drop(redis_instance);
 
     server.write_all(&client_buffer[..rdat_len]).unwrap();
 
@@ -65,13 +86,19 @@ fn handle_client(mut client: TcpStream) {
 }
 
 fn main() {
+    let redis_client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+
 
     let tcp_server = TcpListener::bind("127.0.0.1:3000").unwrap();
     
     println!("[MAIN] Multiplexer on 127.0.0.1:3000");
+
+
     for client in tcp_server.incoming() {
+
+        let mut connection = redis_client.get_connection().unwrap();
         thread::spawn(move || {
-            handle_client(client.unwrap());
+            handle_client(client.unwrap(), connection);
         });
     }
 }
